deeee361e8362c0f379785485249a03e59e441a2
gilvangouvea <37032185+gilvangouvea@users.noreply.github.com>
2020-08-21 14:26:38 -0300

    Create 4A.cpp
    Problema 4A Code Forces

---
+++ 4A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+        int x{};
+        cin>>x;
+        if (x%%4 == 0)
+        {
+            cout<<"YES";
+        }
+        else
+        {
+            cout<<"NO";
+        }
+
+    return 0;
+}
